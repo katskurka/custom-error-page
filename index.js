8ff61a5877fd6b6994dcdbe09a9a12f1d99006bd
katskurka <kati.skurka@gmail.com>
2021-08-02 21:34:20 -0400

    got it working

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+app.all('*', (request, res) => {
+  res.sendFile(__dirname + '/public/404.html')
+})
+
 app.listen(1337)
+// eslint-disable-next-line no-console
+console.log('yay server is up')
